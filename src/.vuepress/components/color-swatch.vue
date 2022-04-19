a9df48ef72423da6e1f107df7b3be6c3c2742739
Matthew Connelly <matthew@harbourshare.com>
2022-04-19 14:34:27 -0400

    initial commit

---
+++ src/.vuepress/components/color-swatch.vue
@@ -0,0 +1,16 @@
+<template>
+    <div :style="{display: 'flex'}">
+      <div :style="{width: width+'px', height: width+'px', backgroundColor: color, marginRight: '1em'}"></div>
+      <p>{{color}}</p>
+    </div>
+</template>
+
+<script>
+  export default {
+    props: ['color', 'width'],
+    data() {
+      return {
+      }
+    }
+  }
+</script>
---
+++ src/.vuepress/components/small-image.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <img :style="{maxWidth: '140px'}" :src="src" />
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['src',],
+    data() {
+      return {
+      }
+    }
+  }
+</script>
